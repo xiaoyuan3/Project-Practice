8027e7d1563cb394d3a5f8eb8cb17217527efb04
xiaoyuan3 <980204468@qq.com>
2020-11-16 14:14:23 +0800

    project

---
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -0,0 +1,63 @@
+<template>
+	<tab-bar>
+		<tab-bar-item path="/home">
+			<template #item-icon>
+				<img src="~assets/images/tabbar/home.svg" alt="">
+			</template>
+			<template #item-icon-active>
+				<img src="~assets/images/tabbar/home_active.svg" alt="">
+			</template>
+			<template #item-text>
+				<div>主页</div>
+			</template>
+		</tab-bar-item>
+		<tab-bar-item path="/sort">
+			<template #item-icon>
+				<img src="~assets/images/tabbar/category.svg" alt="">
+			</template>
+			<template #item-icon-active>
+				<img src="~assets/images/tabbar/category_active.svg" alt="">
+			</template>
+			<template #item-text>
+				<div>分类</div>
+			</template>
+		</tab-bar-item>
+		<tab-bar-item path="cart">
+			<template #item-icon>
+				<img src="~assets/images/tabbar/shopcart.svg" alt="">
+			</template>
+			<template #item-icon-active>
+				<img src="~assets/images/tabbar/shopcart_active.svg" alt="">
+			</template>
+			<template #item-text>
+				<div>购物车</div>
+			</template>
+		</tab-bar-item>
+		<tab-bar-item path="/my">
+			<template #item-icon>
+				<img src="~assets/images/tabbar/profile.svg" alt="">
+			</template>
+			<template #item-icon-active>
+				<img src="~assets/images/tabbar/profile_active.svg" alt="">
+			</template>
+			<template #item-text>
+				<div>我的</div>
+			</template>
+		</tab-bar-item>
+	</tab-bar>
+</template>
+
+<script>
+	import TabBar from "components/common/tabbar/TabBar.vue"
+	import TabBarItem from "components/common/tabbar/TabBarItem.vue"
+	export default {
+		name: 'MainTabBar',
+		components: {
+			TabBar,
+			TabBarItem
+		}
+	}
+</script>
+
+<style>
+</style>
